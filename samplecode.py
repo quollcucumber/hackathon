11e4f066fc0c11a23a74fa74b29c4ee7d86e12d4
Lucas Bosco <lucasbosco1@icloud.com>
2026-03-02 16:29:02 +1100

    Update samplecode.py
    Update code for correlation heatmap

--- samplecode.py
+++ samplecode.py
@@ -4,3 +4,18 @@ import pandas as pd
 df = pd.read_csv()
 correlation = df[].corr(df[])
 print(correlation)
+
+# correlation matrix
+
+plt.imshow(corr)
+plt.colorbar()
+
+for i in range(len(corr.columns)):
+    for j in range(len(corr.columns)):
+        plt.text(j, i, round(corr.iloc[i, j], 2),
+                 ha="center", va="center")
+
+plt.xticks(range(len(corr.columns)), corr.columns, rotation=45)
+plt.yticks(range(len(corr.columns)), corr.columns)
+plt.title("Correlation Heatmap")
+plt.show()
