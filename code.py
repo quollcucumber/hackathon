b8db8adc7f2edc68cf2dc4c5e73c66a43ea58569
JS314271 <632740@trinity.vic.edu.au>
2026-03-03 13:50:46 +1100

    Update code.py

--- code.py
+++ code.py
@@ -1,1 +1,1 @@
-print("See my email")
+df['What grade do you usually achieve overall?'] = df['What grade do you usually achieve overall?'].replace(['A+', 'A', 'B+', 'B', 'C', 'D or below'], [0, 1, 2, 3, 4, 5], inplace=True)
